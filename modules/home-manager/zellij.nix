f922e94bdaac109b294a523cb191adf468dbb208
rafaeldgruenevald <rafaeldgruenevald@proton.me>
2024-07-25 03:39:33 -0300

    31 current  2024-07-25 03:39:31  24.11.20240721.68c9ed8  6.6.41                          *

--- modules/home-manager/zellij.nix
+++ modules/home-manager/zellij.nix
@@ -4,5 +4,10 @@
   programs.zellij = {
     enable = true;
     enableBashIntegration = true;
+    settings = {
+      simplified_ui = true;
+      pane_frames = false;
+      on_force_close = "quit";
+    };
   };
 }
