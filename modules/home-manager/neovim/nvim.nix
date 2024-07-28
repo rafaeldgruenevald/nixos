dd8e8112b960aba60a8824b278d75532137d91a4
rafaeldgruenevald <rafaeldgruenevald@proton.me>
2024-07-28 20:41:19 -0300

    94 current  2024-07-28 20:41:16  24.11.20240721.68c9ed8  6.6.41                          *

--- modules/home-manager/neovim/nvim.nix
+++ modules/home-manager/neovim/nvim.nix
@@ -135,37 +135,37 @@
 	keymaps.extra = [
 	  {
 	    action = {
-	      __raw = "require('telescope.builtin').lsp_definitions()";
+	      __raw = "require('telescope.builtin').lsp_definitions";
 	    };
 	    key = "gd";
 	  }
 	  {
 	    action = {
-	      __raw = "require('telescope.builtin').lsp_references()";
+	      __raw = "require('telescope.builtin').lsp_references";
 	    };
 	    key = "gr";
 	  }
 	  {
 	    action = {
-	      __raw = "require('telescope.builtin').lsp_implementations()";
+	      __raw = "require('telescope.builtin').lsp_implementations";
 	    };
 	    key = "gI";
 	  }
 	  {
 	    action = {
-	      __raw = "require('telescope.builtin').lsp_type_definitions()";
+	      __raw = "require('telescope.builtin').lsp_type_definitions";
 	    };
 	    key = "<leader>D";
 	  }
 	  {
 	    action = {
-	      __raw = "require('telescope.builtin').lsp_document_symbols()";
+	      __raw = "require('telescope.builtin').lsp_document_symbols";
 	    };
 	    key = "<leader>ds";
 	  }
 	  {
 	    action = {
-	      __raw = "require('telescope.builtin').lsp_dynamic_workspace_symbols()";
+	      __raw = "require('telescope.builtin').lsp_dynamic_workspace_symbols";
 	    };
 	    key = "<leader>ds";
 	  }
