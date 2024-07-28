c79e5f5c7efa5f3fc9e133653782849eaf892a85
rafaeldgruenevald <rafaeldgruenevald@proton.me>
2024-07-28 16:53:44 -0300

    61 current  2024-07-28 16:53:42  24.11.20240721.68c9ed8  6.6.41                          *

--- modules/home-manager/neovim/nvim.nix
+++ modules/home-manager/neovim/nvim.nix
@@ -65,6 +65,14 @@
       cmp = {
 	enable = true;
 	autoEnableSources = true;
+
+	settings = {
+	  sources = [
+	    {name = "nvim_lsp";}
+	    {name = "path";}
+	    {name = "buffer";}
+	  ];
+	};
       };
     };
   };
