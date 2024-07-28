87fe982727b68cff223b2f1869347ef04d9618ac
rafaeldgruenevald <rafaeldgruenevald@proton.me>
2024-07-28 16:36:53 -0300

    59 current  2024-07-28 16:36:51  24.11.20240721.68c9ed8  6.6.41                          *

--- modules/home-manager/neovim/nvim.nix
+++ modules/home-manager/neovim/nvim.nix
@@ -16,6 +16,29 @@
     plugins = {
       bufferline.enable = true;
       lualine.enable = true;
+
+      lsp = {
+        enable = true;
+
+	servers = {
+	  # javascript / typescript
+	  tsserver.enable = true;
+	  
+	  # lua
+	  lua-ls.enable = true;
+
+	  # golang
+	  gopls.enable = true;
+
+	  # rust
+	  rust-analyzer.enable = true;
+	};
+      };
+
+      cmp = {
+	enable = true;
+	autoEnableSources = true;
+      };
     };
   };
 }
