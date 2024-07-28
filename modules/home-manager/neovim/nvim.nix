56a54447bd56afdc73262dd11ccd5f3b36484590
rafaeldgruenevald <rafaeldgruenevald@proton.me>
2024-07-28 15:12:10 -0300

    49 current  2024-07-28 15:12:08  24.11.20240721.68c9ed8  6.6.41                          *

--- modules/home-manager/neovim/nvim.nix
+++ modules/home-manager/neovim/nvim.nix
@@ -4,6 +4,6 @@
   stylix.targets.vim.enable = false;
   programs.neovim = {
     enable = true;
-    extraConfig = lib.fileContents ./nvim/init.lua; 
+    extraLuaConfig = lib.fileContents ./nvim/init.lua; 
   };
 }
