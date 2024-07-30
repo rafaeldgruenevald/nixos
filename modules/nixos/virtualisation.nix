4d836b07ccd77ea417615edc24254bf92cfd4511
rafaeldgruenevald <rafaeldgruenevald@proton.me>
2024-07-30 18:45:59 -0300

    130 current  2024-07-30 18:45:54  24.11.20240721.68c9ed8  6.6.41                          *

--- modules/nixos/virtualisation.nix
+++ modules/nixos/virtualisation.nix
@@ -4,6 +4,11 @@
   # Virt-Manager
   virtualisation.libvirtd.enable = true; 
   programs.virt-manager.enable = true;
+  # Virtualbox
+  virtualisation.virtualbox.host.enable = true;
+  environment.systemPackages = with pkgs; [
+    virtualbox
+  ];
   # Docker
   virtualisation.docker.enable = true;
 }
